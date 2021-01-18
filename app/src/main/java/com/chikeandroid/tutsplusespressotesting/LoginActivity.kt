b961f03a088db441efc488a35fa64e3f688ea31d
Julia1999000 <jyliu1999@gmail.com>
2021-01-19 01:48:49 +0300

    ui-тестирование с espresso

---
+++ app/src/main/java/com/chikeandroid/tutsplusespressotesting/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.chikeandroid.tutsplusespressotesting
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var loginTitleTextView: TextView
+    private lateinit var passwordEditText: EditText
+    private lateinit var submitButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        usernameEditText = findViewById(R.id.et_username)
+        passwordEditText = findViewById(R.id.et_password)
+        submitButton = findViewById(R.id.btn_submit)
+        loginTitleTextView = findViewById(R.id.tv_login)
+
+        submitButton.setOnClickListener {
+            if (usernameEditText.text.toString() == getString(R.string.nameUser) &&
+                    passwordEditText.text.toString() == getString(R.string.passwordUser)) {
+                loginTitleTextView.text = getString(R.string.txtSuccess)
+            } else {
+                loginTitleTextView.text = getString(R.string.txtFailure)
+            }
+        }
+    }
+}
